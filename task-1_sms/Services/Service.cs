54ee91d7e09bee9b6a8f6bb465ffff890ef89fd1
flabioassuncao <flabiofilho@gmail.com>
2022-07-19 15:27:16 +0100

    crested a new method to find the maximum product

--- task-1_sms/Services/Service.cs
+++ task-1_sms/Services/Service.cs
@@ -14,12 +14,12 @@ public static class Service
         var finalResult = 0;
         for (var i = 0; i < numbers.Length - 1; i++)
         {
-            for (var j = i + 1 ; j < numbers.Length; j++)
+            for (var j = i + 1; j < numbers.Length; j++)
             {
                 var result = numbers[i] * numbers[j];
-                
+
                 if (finalResult >= result) continue;
-                
+
                 if (result.IsDivisibleBy3())
                 {
                     finalResult = result;
@@ -28,5 +28,31 @@ public static class Service
         }
 
         return finalResult;
+    }
+
+    /// <summary>
+    /// This method will find the maximum product between two numbers from the array, that is a multiple of 3. (liner complexity)
+    /// </summary>
+    /// <param name="numbers"></param>
+    /// <returns>int</returns>
+    public static int FindTheMaximumProductBetweenTwoNumbersFromArray2(int[] numbers)
+    {
+        var finalResult = 0;
+
+        Array.Sort(numbers);
+
+        var multiplesOf3 = numbers.Where(c => c.IsDivisibleBy3()).ToArray();
+        
+        var nonMultiplesOf3 = numbers.Where(c => !c.IsDivisibleBy3()).ToArray();
+        
+        if (multiplesOf3.Length > 1)
+        {
+            // the result of multiple of 3 * multiples of 3 is a multiples of 3
+            finalResult = multiplesOf3[^1] * multiplesOf3[^2];
+        }
+
+        var tempResult = multiplesOf3[^1] * nonMultiplesOf3[^1];
+
+        return tempResult > finalResult ? tempResult : finalResult;
     }
 }
--- task-1_sms_tests/UnitTest1.cs
+++ task-1_sms_tests/UnitTest1.cs
@@ -8,9 +8,11 @@ public class UnitTest1
     [Theory]
     [InlineData(new[] {6, 8, 8, 7, 2, 5}, 48)]
     [InlineData(new[] {1, 9, 2, 4}, 36)]
+    [InlineData(new[] {1, 9, 2, 4, 6}, 54)]
     public void Should_FindTheMaximumProductBetweenTwoNumbers_FromTheArray_ThatIsMultipleOf3(int[] numbers, int expectedResult)
     {
-        var result = Service.FindTheMaximumProductBetweenTwoNumbersFromArray(numbers);
+        // var result = Service.FindTheMaximumProductBetweenTwoNumbersFromArray(numbers);
+        var result = Service.FindTheMaximumProductBetweenTwoNumbersFromArray2(numbers);
         Assert.Equal(expectedResult, result);
     }
 }
